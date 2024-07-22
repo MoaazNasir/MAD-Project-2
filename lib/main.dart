f1a1da61140ba8944e11d9b198456a9048edd843
sophianoh <sophiaanoh@gmail.com>
2024-07-22 15:16:19 -0400

    uiux dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,294 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ArtfolioApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ArtfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Artfolio',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/gallery': (context) => GalleryScreen(),
+        '/artwork-details': (context) => ArtworkDetailsScreen(),
+        '/artist-profile': (context) => ArtistProfileScreen(),
+        '/upload': (context) => UploadScreen(),
+        '/feedback': (context) => FeedbackScreen(),
+        '/search': (context) => SearchScreen(),
+        '/user-profile': (context) => UserProfileScreen(),
+        '/create-user': (context) => CreateUserScreen(),
+        '/edit-user': (context) => EditUserScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// Home Screen
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Artfolio Home'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/gallery'),
+            child: Text('Go to Gallery'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/upload'),
+            child: Text('Upload Artwork'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/search'),
+            child: Text('Search Artworks'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/user-profile'),
+            child: Text('My Profile'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Gallery Screen
+class GalleryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemCount: 10, // Replace with actual data length
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, '/artwork-details'),
+            child: Card(
+              child: Center(child: Text('Artwork $index')),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Artwork Details Screen
+class ArtworkDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Artwork Details'),
+      ),
+      body: Column(
+        children: [
+          Text('Artwork Title'),
+          Text('Artwork Description'),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/feedback'),
+            child: Text('Leave Feedback'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Artist Profile Screen
+class ArtistProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Artist Profile'),
+      ),
+      body: Column(
+        children: [
+          Text('Artist Biography'),
+          // Display artist's artworks here
+        ],
+      ),
+    );
+  }
+}
+
+// Upload Screen
+class UploadScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload Artwork'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Tags'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle upload
+              },
+              child: Text('Upload'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// Feedback Screen
+class FeedbackScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Feedback'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: 'Leave a comment'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Handle feedback submission
+            },
+            child: Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Search Screen
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+      ),
+      body: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search for artworks or artists',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+}
+
+// User Profile Screen
+class UserProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
+      body: Column(
+        children: [
+          Text('User Name'),
+          Text('User Bio'),
+          ElevatedButton(
+            onPressed: () => Navigator.pushNamed(context, '/edit-user'),
+            child: Text('Edit Profile'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Create User Screen
+class CreateUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Account'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle user creation
+                Navigator.pushNamed(context, '/');
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Edit User Screen
+class EditUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Bio'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle profile update
+              },
+              child: Text('Save Changes'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
